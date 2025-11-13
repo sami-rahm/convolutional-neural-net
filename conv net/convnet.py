c92962e7b5a5a60a596abc586a546361ed59773d
sami-rahm <25sami.rah@gmail.com>
2025-11-13 22:10:19 +0000

    Refactor convolutional neural network visualization
    convolution visualization added but the hardest part is next (back propagation). also code is kinda messy and could be improved. some bugs here and there

--- conv net/convnet.py
+++ conv net/convnet.py
@@ -1,13 +1,14 @@
 import numpy as np
 from PIL import Image
 import pygame
+import random
 
 pygame.init()
 font=pygame.font.Font("SpaceMono-Bold.ttf",15)
 
 #allowing multiple images (input images, feature maps, predicted images) for better visualization
 
-imgcountH=3
+imgcountH=2
 imgcountW=2
 
 img_pad_x=40
@@ -31,16 +32,18 @@ pygame.display.set_icon(icon)
 class filter3x3: # the kernel that slides over the image to create a feature map
     def __init__(self):
         self.filter=np.array([
-            [1,0,1],
-            [1,0,1],
-            [1,0,1]],dtype=np.float16) # numbers represent weights to multiply the pixel value by
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]],dtype=np.float16) # numbers represent weights to multiply the pixel value by
         
 class img:
     def __init__(self,filepath=r"media\test_img.png",index=1): # index represents the position in the window (top left to bottom right)
         
         self.filepath=filepath 
 
-        self.img=np.array(Image.open(filepath),dtype=np.float16)/255 if filepath is not None else None# normalized image values
+        self.unprocessed_img=Image.open(filepath)
+
+        self.img=np.array(self.unprocessed_img,dtype=np.float16)/255 if filepath is not None else None# normalized image values
 
         self.height,self.width=self.img.shape # image height, width
 
@@ -50,7 +53,7 @@ class img:
 
         self.tlx=0 #top left rect corner
         self.tly=0
-        adj_index=int(self.index/2)
+        adj_index=self.index//2
         self.tly=img_pad_y*(adj_index+1)+imgh*(adj_index)*scale_fac
 
 
@@ -79,8 +82,56 @@ class img:
         win.blit(self.text,(self.tlx,self.tly-img_pad_y/1.5)) # add image label eg input image, feature map 1 etc
     
 class conv_img: #img with added functions like passing a kernel over the image to create a feature map
-    def __init__(self):
-        pass        
+    def __init__(self,image,fil):
+        self.input_img=image
+        self.filter=fil
+        self.stride=1
+
+        self.filterw=3
+        self.filterh=3
+
+        self.padx=0
+        self.pady=0
+
+        self.width=int( (self.input_img.width + 2*self.padx-self.filterw)/self.stride + 1)
+        self.height=int( (self.input_img.height + 2*self.pady-self.filterh)/self.stride + 1)
+
+        self.output_img=np.array([[0 for _ in range(self.width)] for _ in range(self.height)],dtype=np.float16)
+
+        self.label="feature map"
+
+        self.text_colour=(222, 215, 202)
+        self.text=font.render(self.label,True,self.text_colour)
+
+    def produce_image(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                value=0
+                for fh in range(self.filterh):
+                    for fw in range(self.filterw):
+                        value+=self.filter.filter[fh][fw]*self.input_img.img[h*self.stride+fh][w*self.stride+fw]
+                #print(value)
+                self.output_img[h][w]=value
+        self.draw()
+
+    def draw(self):
+
+        pygame.draw.rect(win,(255,255,255),(self.input_img.tlx,self.input_img.tly,28*5,28*5))
+
+        for y in range(self.height):
+            for x in range(self.width):
+                
+                pix=max(0,self.output_img[y][x]/(self.output_img.max()+0.001))*255
+                pix_x=self.input_img.tlx+x*scale_fac #pixel top left corner co - ordinate
+                pix_y=self.input_img.tly+y*scale_fac
+
+                pygame.draw.rect(win,(pix,pix,pix),(pix_x,pix_y,scale_fac,scale_fac))
+
+
+        win.blit(self.text,(self.input_img.tlx,self.input_img.tly-img_pad_y/1.5)) # add image label eg input image, feature map 1 etc
+                    
+
+          
         
 
 
@@ -88,7 +139,12 @@ class conv_img: #img with added functions like passing a kernel over the image t
         
 
 imgs=[img(index=i) for i in range(imgcountW*imgcountH)]
+fils=[filter3x3() for _ in range(3)]
+cimgs=[conv_img(imgs[i+1],fils[i]) for i in range(3)]
 
+
+
+count=0
 run=True     
 while run:
     for event in pygame.event.get():
@@ -96,7 +152,17 @@ while run:
             run=False
 
     win.fill(bg_colour)
-    for img in imgs: #draw all images
-        img.draw()
+
+    if count%10==0:
+        fils[random.randint(0,2)].filter= np.array([
+            [5-random.random()*10,5-random.random()*10,5-random.random()*10],
+            [5-random.random()*10,5-random.random()*10,5-random.random()*10],
+            [5-random.random()*10,5-random.random()*10,5-random.random()*10]],dtype=np.float16)
+
+
+    imgs[0].draw()
+    for cimg in cimgs:
+        cimg.produce_image()
 
     pygame.display.flip()
+    count+=1
